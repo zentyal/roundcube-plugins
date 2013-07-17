820f7ddeeb6cc0f49b8330b401a0ee957b0e6e61
Miguel Julián <mjulian@zentyal.com>
2013-07-17 23:12:08 +0200

    Deleted outdated plugins

--- calendar/program/js/move_button.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){rcmail.addEventListener("init",function(){try{$("#calendar_button").insertBefore("#"+rcmail.buttons.settings[0].id)}catch(a){}$("#calendar_button").show()})});
